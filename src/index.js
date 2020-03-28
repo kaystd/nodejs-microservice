432d38f2983e25b44f7913795c58076a4d80ec6d
Danil Kaist <kaystd@gmail.com>
2020-03-28 16:30:08 +0300

    Express helloworld

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+const express = require('express')
+const application = express()
+const port = 8080
+
+application.get('/', (request, response) => {
+  response.send("Welcome to Node.js microservice!")
+})
+
+application.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`)
+})
+
