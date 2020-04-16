feeade48bf395d23aaf79e1f5b0753472e92dc6a
Danil Kaist <kaystd@gmail.com>
2020-04-16 10:45:36 +0300

    Add express JSON body parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express'
 import express from  'express'
 import Auth from './middleware/Auth'
-import * as dotenv from 'dotenv'
+import dotenv from 'dotenv'
 
 dotenv.config()
 const application = express()
 const port = process.env.PORT
+application.use(express.json())
 
 application.get('/', Auth.verifyToken, (request: Request, response: Response) => {
   response.send("Welcome to Node.js microservice!")
