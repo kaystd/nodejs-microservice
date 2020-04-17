61a724d750111266d02a13777c5fcb6856b6675a
Danil Kaist <kaystd@gmail.com>
2020-04-17 12:36:40 +0300

    Add get key route

--- src/routes/keyRoutes.ts
+++ src/routes/keyRoutes.ts
@@ -3,19 +3,34 @@ import { Db } from 'mongodb'
 import Auth from '../middleware/Auth'
 
 export default (app: Application, db: Db): void => {
-  app.post('/key', Auth.verifyToken, (request: Request, response: Response) => {
+  app.post('/key/get', (request: Request, response: Response) => {
+    if (!request.body.mail) {
+      response.send({ error: 'Mail is not provided' })
+    } else {
+      const id = { name: 'RSA-public' }
+      db.collection('keys').findOne(id, (err, result) => {
+        if (err) {
+          response.send({ error: err.message })
+        } else {
+          response.send({ key: result.key })
+        }
+      })
+    }
+  })
+  app.post('/key/send', Auth.verifyToken, (request: Request, response: Response) => {
     if (!request.body.key) {
       response.send({ error: 'Key is not provided' })
+    } else {
+      const id = { name: 'RSA-public' }
+      const key = { $set: { name: 'RSA-public', key: request.body.key } }
+      const upsert = { upsert: true }
+      db.collection('keys').updateOne(id, key, upsert, (err, result) => {
+        if (err) {
+          response.send({ error: err.message })
+        } else {
+          response.send({ successModified: result.upsertedCount + result.modifiedCount })
+        }
+      })
     }
-    const id = { name: 'RSA-public' }
-    const key = { $set: { name: 'RSA-public', key: request.body.key } }
-    const upsert = { upsert: true }
-    db.collection('keys').updateOne(id, key, upsert, (err, result) => {
-      if (err) {
-        response.send({ error: err.message })
-      } else {
-        response.send({ successModified: result.upsertedCount })
-      }
-    })
   })
 }
